db29b4c35f7c372e86f52836fb62dc35bdd969bb
remaleino <raimond.leino@gmail.com>
2022-09-19 21:16:22 +0300

    Part 4

--- Osa_4/blogilista/controllers/blogs.js
+++ Osa_4/blogilista/controllers/blogs.js
@@ -1,22 +1,37 @@
-const logger = require('../utils/logger')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
     const blog = new Blog(request.body)
-    logger.info('here')
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    try {
+        const savedBlog = await blog.save()
+        response.status(201).json(savedBlog)
+    } catch (exception) {
+        next(exception)
+    }
 })
-
+blogsRouter.get('/:id', async (request, response, next) => {
+    try {
+        const blog = await Blog.findById(request.params.id)
+        if (blog) {
+            response.json(blog)
+        } else {
+            response.status(404).end()
+        }
+    } catch (exception) {
+        next(exception)
+    }
+})
+blogsRouter.get('/:id', async (req, res, next) => {
+    try {
+        await Blog.findByIdAndRemove(req.params.id)
+        res.status(204).end()
+    } catch (e) {
+        next(e)
+    }
+})
 module.exports = blogsRouter
--- Osa_4/blogilista/tests/blog_api.test.js
+++ Osa_4/blogilista/tests/blog_api.test.js
@@ -1,40 +1,82 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-const initialBlogs = [
-    {
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5
-    },
-    {
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5
-    }
-]
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
+    let blogObject = new Blog(helper.initialBlogs[0])
     await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
+    blogObject = new Blog(helper.initialBlogs[1])
     await blogObject.save()
 })
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
 })
-test('there are two notes', async () => {
+test('there are two blogs', async () => {
     const res = await api.get('/api/blogs')
-    expect(res.body).toHaveLength(initialBlogs.length)
+    expect(res.body).toHaveLength(helper.initialBlogs.length)
+})
+//4.8
+test('all blogs are returned', async () => {
+    const res = await api.get('/api/blogs')
+    expect(res.body).toHaveLength(helper.initialBlogs.length)
+})
+//4.9
+test('an id is defined in blogs', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const idDefinitions = blogsAtStart.map(r => r.id)
+    expect(idDefinitions).toHaveLength(helper.initialBlogs.length)
+})
+//4.10
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: 'test',
+        author: 'test',
+        url: 'test',
+        likes: 1
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const titles = blogsAtEnd.map(r => r.title)
+    expect(titles).toContain('test')
+})
+test('note without id is not added', async () => {
+    const newBlog = {
+        title: 'id-test',
+        author: 'test',
+        url: 'test',
+        likes: 1
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+test('a spesific note can be viewed', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToView = blogsAtStart[0]
+
+    const resultBlog = await api
+        .get(`/api/blog/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    const processedBlogToView = JSON.parse(JSON.stringify(blogToView))
+    expect(resultBlog.body).toEqual(processedBlogToView)
 })
 afterAll(() => {
     mongoose.connection.close()
